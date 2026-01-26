a20791665311150d3720af107ac6c49043923152
gabrielbelloo <167886489+gabrielbelloo@users.noreply.github.com>
2026-01-26 15:08:07 -0300

    feat: add "ResultsList.jsx" component
    add "ResultsList.jsx" component

--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -1,9 +1,11 @@
+import ResultsList from "./ResultsList"
 import Upload from "./Upload"
 
 export default function Container(){
     return (
-        <div className="bg-gray-800/50 p-5 rounded-lg outline outline-gray-700/50 backdrop-blur-sm text-white">
+        <div className="bg-gray-800/40 p-5 rounded-lg outline outline-gray-700/50 backdrop-blur-sm text-white gap-8 flex flex-col">
             <Upload />
+            <ResultsList />
             </div>
     )
 }
